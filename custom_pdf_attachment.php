db4bd3ec2a1614de93961a85bb58d0adf1915e97
avi.megladon <avi.megladon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-13 17:33:46 +0000

    new release..
    git-svn-id: https://plugins.svn.wordpress.org/custom-post-type-pdf-attachment/tags/1.0.1@874889 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ custom_pdf_attachment.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Plugin Name: Custom Post Type Attachment ( PDF )
+Plugin URI: http://avifoujdar.wordpress.com/category/my-wp-plugins/
+This plugin will allow you to upload pdf files to your post or pages or any other custom post types. You can either use shortcodes or functions to display attachments. You can upload at most 10 PDF files as attachments. :)
+Version: 1.0.1
+Author: AFO
+Author URI: http://avifoujdar.wordpress.com/
+*/
+
+/**
+	  |||||   
+	<(`0_0`)> 	
+	()(afo)()
+	  ()-()
+**/
+
+include_once dirname( __FILE__ ) . '/pdf_attachment.php';
+include_once dirname( __FILE__ ) . '/shortcode_function.php';
+
+add_action('admin_menu', 'custom_pdf_attachment_plugin_menu');
+
+function custom_pdf_attachment_plugin_menu() {  
+  add_options_page( 'Custom Pdf Attachment', 'Custom PDF Attachment', 1, 'custom_pdf_attachment', 'custom_pdf_attachment_options');
+}
+
+function custom_pdf_attachment_options() {
+global $wpdb;
+
+$saved_types = get_option('saved_post_types_for_pdf_attachment');
+$saved_no_of_pdf_attachment = get_option('saved_no_of_pdf_attachment');
+?>
+
+<table width="98%" border="0" style="background-color:#FFFFD2; border:1px solid #FFFF00;">
+ <tr>
+ <td align="right"><h3>Even $0.60 Can Make A Difference</h3></td>
+    <td><form name="_xclick" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+          <input type="hidden" name="cmd" value="_xclick">
+          <input type="hidden" name="business" value="avifoujdar@gmail.com">
+          <input type="hidden" name="item_name" value="Donation for plugins">
+          <input type="hidden" name="currency_code" value="USD">
+          <input type="hidden" name="amount" value="0.60">
+          <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="Make a donation with PayPal">
+        </form></td>
+  </tr>
+</table>
+<form name="f" method="post" action="">
+<input type="hidden" name="option" value="save_custom_pdf_attachment_settings" />
+<table width="100%" border="0">
+  <tr>
+    <td><h1>Settings</h1></td>
+  </tr>
+  <tr>
+    <td><strong>Select Post Types ( You want custom attachments )</strong></td>
+  </tr>
+  <tr>
+    <td><?php
+	$args = array(
+   	'public'   => true,
+	);
+	$post_types = get_post_types( $args, 'names' ); 
+	$post_types = array_diff($post_types, array('attachment'));
+	foreach ( $post_types as $post_type ) {
+		if(is_array($saved_types) and in_array($post_type,$saved_types)){
+			echo '<p><input type="checkbox" name="attachment_post_types[]" value="'.$post_type.'" checked="checked" />&nbsp;'.$post_type.'</p>';
+		} else{
+			echo '<p><input type="checkbox" name="attachment_post_types[]" value="'.$post_type.'" />&nbsp;'.$post_type.'</p>';
+		}
+	}
+	?></td>
+  </tr>
+  <tr>
+    <td><strong>Number of attachment Files</strong></td>
+  </tr>
+  <tr>
+    <td><?php	
+		echo '<select name="no_of_pdf_attachment">';
+			echo '<option value="">--</option>';
+			for($i=1; $i<=10;$i++){
+				if($saved_no_of_pdf_attachment == $i){
+					echo '<option value="'.$i.'" selected="selected">'.$i.'</option>';
+				} else {
+					echo '<option value="'.$i.'">'.$i.'</option>';
+				}
+			}
+		echo '</select>';
+	?></td>
+  </tr>
+  
+  <tr>
+    <td><?php	
+		if($saved_no_of_pdf_attachment > 0 and $saved_no_of_pdf_attachment <= 10){
+			echo '<table width="100%" border="0">';
+			for($i=1; $i<=$saved_no_of_pdf_attachment;$i++){
+				echo '<tr>
+					<td><strong>Shortcode :</strong> <span style="color:#FF0000">[pdf_attachment file="'.$i.'" name="optional file_name"]</span></td>
+					<td><strong>Custom function :</strong> <span style="color:#FF0000">&lt;?php echo pdf_attachment_file('.$i.',"optional file_name");?&gt;</span></td>
+				  </tr>';
+			}
+			echo '</table>';
+		}
+	?></td>
+  </tr>
+  
+  <tr>
+    <td><input type="submit" name="submit" value="Save" /></td>
+  </tr>
+</table>
+</form>
+<?php 
+} // end of function 
+
+
+function custom_pdf_attachment_post_date(){
+	if($_POST['option'] == "save_custom_pdf_attachment_settings"){
+		update_option( 'saved_post_types_for_pdf_attachment', $_POST['attachment_post_types'] );
+		update_option( 'saved_no_of_pdf_attachment', $_POST['no_of_pdf_attachment'] );
+	}
+}
+
+add_action( 'admin_init', 'custom_pdf_attachment_post_date' );
+
+register_activation_hook(__FILE__,'plug_install_custom_pdf_attachment');
+register_deactivation_hook(__FILE__,'plug_unins_custom_pdf_attachment');
+function plug_install_custom_pdf_attachment(){}
+function plug_unins_custom_pdf_attachment(){}
---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Custom Post Type Attachment ( PDF ) ===
+Contributors: afo
+Donate link: http://donateafo.net84.net/
+Tags: attachment, post attachment, pdf attachment, upload file, pdf in post, pdf in custom post type, post pdf, file attachment, file, pdf
+Requires at least: 2.0.2
+Tested up to: 3.8.1
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin will allow you to upload pdf files to your post or pages or any other custom post types.
+
+
+== Description ==
+
+* This plugin will allow you to upload pdf files to your post or pages or any other custom post types.
+* You can eather use shortcodes or functions to display attachments. 
+* At most 10 PDF files can be uploaded.
+* just install the plugin and update setting from `Settings-> Custom Pdf Attachment`. 
+* Upload files in your post/page edit page.
+* Display files in frontend using shortcodes or using custom functions.
+
+= Usage =
+* [pdf_attachment file="file_number" name="optional file_name"]
+* Or
+* pdf_attachment_file("file_number","optional file_name");
+* Example: pdf_attachment_file(1,"PDF File 1");
+
+== Installation ==
+
+
+1. Upload `custom-pos-type-pdf-attachment.zip` to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to `Settings-> Custom Pdf Attachment`, and set some options. It's really easy.
+4. Use shortcodes or functions to display uploaded files in frontend.
+
+
+== Frequently Asked Questions ==
+
+= For any kind of problem =
+
+1. Please email me avi.megladon@gmail.com. My site is http://avifoujdar.wordpress.com/contact/ 
+
+
+== Screenshots ==
+
+1. Frontend view
+2. Settings page view
+3. Post edit page view
+
+
+== Changelog ==
+
+= 1.0.1 =
+* this is the first release.
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+I will update this plugin when ever it is required.
