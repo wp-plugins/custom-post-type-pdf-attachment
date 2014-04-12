98b03226c78e8066508f026def8c1f6b560b27b3
avi.megladon <avi.megladon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-12 08:04:58 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/custom-post-type-pdf-attachment/tags/1.0.1@891757 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom_pdf_attachment.php
+++ custom_pdf_attachment.php
@@ -4,7 +4,7 @@ Plugin Name: Custom Post Type Attachment ( PDF )
 Plugin URI: http://avifoujdar.wordpress.com/category/my-wp-plugins/
 This plugin will allow you to upload pdf files to your post or pages or any other custom post types. You can either use shortcodes or functions to display attachments. You can upload at most 10 PDF files as attachments. :)
 Version: 1.0.1
-Author: AFO
+Author: avimegladon
 Author URI: http://avifoujdar.wordpress.com/
 */
 
